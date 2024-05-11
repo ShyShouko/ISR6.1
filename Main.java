b0f99458f0e30ae1887475535a050d6c94688d91
ShyShouko <denis1195@gmail.com>
2024-05-11 18:09:16 +0300

    second update, dev branch

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.print("First update");
+        System.out.print("Second update");
+
 
 
     }
